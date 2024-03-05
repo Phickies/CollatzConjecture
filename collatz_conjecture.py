bb609c383b46f17a1262ea0687bb9eb14f4ee819
Phickies <51713512+Phickies@users.noreply.github.com>
2024-03-05 07:57:33 +0100

    finish

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,3 +1,49 @@
+import pygame
+import math
+
+
+class Collatz:
+
+    def __init__(self, number):
+        while number != 1:
+            number = collatz(number)
+            self.collatz_list = []
+            self.collatz_list.append(number)
+
+    def visual_display(self, screen, start_position: pygame.Vector2, length, width, direction_angle, rotation_angle):
+        # Remove 2 first element for reduce overlap -3
+        for i in range(len(self.collatz_list) - 3, -1, -1):
+            if self.collatz_list[i] % 2 == 0:
+                direction_angle += rotation_angle
+            elif self.collatz_list[i] == 1:
+                direction_angle -= rotation_angle
+            else:
+                direction_angle -= rotation_angle * 2
+
+            # Calculating the end position
+            next_x = start_position.x + math.cos(math.radians(direction_angle)) * length
+            next_y = start_position.y + math.sin(math.radians(direction_angle)) * length
+
+            # Draw a line
+            line = pygame.Surface((length, width), pygame.SRCALPHA)
+
+            # Adding color
+            red = pygame.Color(255, 20, 20, 10)
+            color = red.lerp((255, 255, 20, 10), map_value(start_position.y, screen.get_height(), 0, 0, 1))
+            color = color.lerp((0, 200, 0, 1), map_value(start_position.x, screen.get_width(), -screen.get_width() / 2, 0, 1))
+            line.fill(color)
+
+            # Rotate it with angle and shift it
+            rotated_line = pygame.transform.rotate(line, -direction_angle)
+
+            # draw the rotated line onto the screen
+            screen.blit(rotated_line, (start_position.x, start_position.y))
+
+            # Shift the next start position of the line to the head
+            start_position.x = next_x
+            start_position.y = next_y
+
+
 def collatz(n: int) -> int:
     if n % 2 == 0:
         return int(n/2)
@@ -11,3 +57,30 @@ def collatz_step_count(n: int) -> int:
         n = collatz(n)
         step_count += 1
     return step_count
+
+
+def map_value(value, from_low, from_high, to_low, to_high):
+    """
+    Map a value from one range to another.
+
+    :param value: The value to be mapped.
+    :param from_low: The minimum value of the original range.
+    :param from_high: The maximum value of the original range.
+    :param to_low: The minimum value of the target range.
+    :param to_high: The maximum value of the target range.
+    :return: The mapped value.
+    """
+    if value < from_high and value < from_low:
+        value = from_high
+    if value > from_high and value > from_low:
+        value = from_high
+    if value < from_low and value < from_high:
+        value = from_low
+    if value > from_low and value > from_high:
+        value = from_low
+    from_range = from_high - from_low
+    to_range = to_high - to_low
+
+    scaled_value = float(value - from_low) / float(from_range)
+
+    return to_low + (scaled_value * to_range)
--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-import sys
-import math
-import pygame
 from collatz_conjecture import *
-
-# Set up color
-black = (0, 0, 0)
-white = (255, 255, 255)
-cyan = (0, 200, 200)
-green = (0, 200, 0)
-red = (200, 0, 0)
+import sys
 
 # Setup input
 input_list = []
-for i in range(1, 100000):
+for i in range(1, 50000):
     input_list.append(i)
 
-# Setup length of the segment
-length = 1
-# Setup width
-width = 1
-# Setup angle
-coefficient = 3
 
 # Setup canvas size
-size = (800, 600)
+size = (800, 800)
 
 # State
 is_running = True
@@ -40,33 +25,17 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # screen.fill(black)
-
     # Run loop
     if is_running:
         for input_n in input_list:
-            print(input_n)
-            collatz_list = []
+
             # Setup initial position
-            position = pygame.Vector2(10, 300)
-            angle = 0
+            position = pygame.Vector2(size[0]/2, size[1]-1)
 
-            while input_n != 1:
-                input_n = collatz(input_n)
-                collatz_list.append(input_n)
+            clz = Collatz(input_n)
+            clz.visual_display(screen, position, 5, 2, 270, 6.5)
 
-            for i in range(len(collatz_list)-1, -1, -1):
-                if collatz_list[i] % 2 == 0:
-                    angle += coefficient
-                else:
-                    angle -= coefficient*2
-                next_x = position.x + math.cos(math.radians(angle)) * length
-                next_y = position.y + math.sin(math.radians(angle)) * length
-                pygame.draw.line(screen, white, position, (next_x, next_y), width)
-                position.x = next_x
-                position.y = next_y
+            pygame.display.flip()
 
         is_running = False
-
-        pygame.display.flip()
-        pygame.time.Clock().tick(60)
+    pygame.time.Clock().tick(60)
